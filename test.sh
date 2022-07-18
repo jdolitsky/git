e2393ab445ef7a39e1e005ef68e9e94f112ce28a
Josh Dolitsky <josh@dolit.ski>
2022-07-18 16:13:10 -0500

    Add smoketest
    Signed-off-by: Josh Dolitsky <josh@dolit.ski>

---
+++ test.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -o errexit -o nounset -o errtrace -o pipefail
+
+IMAGE_NAME=${IMAGE_NAME:-"distroless.dev/git"}
+CLONE_URL=${CLONE_URL:-"https://github.com/distroless/git.git"}
+
+rm -rf smoketest
+mkdir smoketest
+trap "rm -rf smoketest" EXIT
+
+# Try cloning a repo and check for README.md
+docker run --rm -v "${PWD}/smoketest":/w -w /w $IMAGE_NAME clone --depth 1 $CLONE_URL .
+find smoketest/README.md
