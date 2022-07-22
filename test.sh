bd2a4f1e678de9c320540d350069bf2c03df6446
Josh Dolitsky <josh@dolit.ski>
2022-07-22 14:30:34 -0500

    delete smoketest dir in the action
    Signed-off-by: Josh Dolitsky <josh@dolit.ski>

--- test.sh
+++ test.sh
@@ -8,10 +8,7 @@ CLONE_URL=${CLONE_URL:-"https://github.com/distroless/git.git"}
 rm -rf smoketest
 mkdir smoketest
 chmod go+wrx smoketest
-
-# TODO: re-enable this
-# has a permissions issue
-#trap "rm -rf smoketest" EXIT
+trap "rm -rf smoketest" EXIT
 
 # Try cloning a repo and check for README.md
 docker run --rm -v "${PWD}/smoketest":/w -w /w $IMAGE_NAME clone --depth 1 $CLONE_URL .
